3c34221749f153200ff04b1a6a53e53c26d2bacf
AnanthaKannan <sreeananthakannan@gmail.com>
2023-12-29 20:47:25 +0530

    config updated

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const port = app.get(ConfigService).get<number>('PORT');
+  await app.listen(port);
 }
 bootstrap();
